8d3eba53eaf30f6015ff5f91be61fe1a9b52d644
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-17 17:20:31 +0300

    fix: connect
    fix typo in connectDB method

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
-    userCreateIndex: true,
+    useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   });
